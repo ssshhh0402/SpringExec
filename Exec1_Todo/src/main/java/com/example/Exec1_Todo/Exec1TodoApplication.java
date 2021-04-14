ce688783df76ce8d2142c845d69774711a68d9f6
ssshhh0402 <ssshhh0402@naver.com>
2021-04-15 00:25:56 +0900

    시작

---
+++ Exec1_Todo/src/main/java/com/example/Exec1_Todo/Exec1TodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Exec1_Todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exec1TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exec1TodoApplication.class, args);
+	}
+
+}
---
+++ Exec1_Todo/src/test/java/com/example/Exec1_Todo/Exec1TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Exec1_Todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exec1TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
