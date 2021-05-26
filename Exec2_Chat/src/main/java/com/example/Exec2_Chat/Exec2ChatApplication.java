9b4c58b50ac95001e0011305b1bb7394b34c0d1a
ssshhh0402 <ssshhh0402@naver.com>
2021-05-26 18:09:50 +0900

    20210526 실시간채팅시작

---
+++ Exec2_Chat/src/main/java/com/example/Exec2_Chat/Exec2ChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.Exec2_Chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exec2ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exec2ChatApplication.class, args);
+	}
+
+}
---
+++ Exec2_Chat/src/test/java/com/example/Exec2_Chat/Exec2ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Exec2_Chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exec2ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
