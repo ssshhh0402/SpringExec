aa56c80a911de9bcac7df481919ffef19fbf1803
ssshhh0402 <ssshhh0402@naver.com>
2022-01-18 00:10:59 +0900

    20210118

--- Exec5_NoJpa/src/main/java/com/example/Exec5_NoJpa/web/UserController.java
+++ Exec5_NoJpa/src/main/java/com/example/Exec5_NoJpa/web/UserController.java
@@ -1,14 +1,16 @@
 package com.example.Exec5_NoJpa.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/hello")
-public class HelloController {
-    @GetMapping("")
-    public String hello(){
-        return "HELLO";
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    @GetMapping("/getUser")
+    public String getUser(){
+        return "USER";
     }
 }
