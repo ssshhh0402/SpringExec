eb5d7760d78c710d87c50ccde767208155f8bd82
ssshhh0402 <ssshhh0402@naver.com>
2022-01-29 02:30:50 +0900

    20210129

--- Exec1_Todo/src/main/java/com/example/Exec1_Todo/Exec1TodoApplication.java
+++ Exec1_Todo/src/main/java/com/example/Exec1_Todo/Exec1TodoApplication.java
@@ -1,12 +1,14 @@
 package com.example.Exec1_Todo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.core.SpringVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Exec1TodoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("+++++++++++"+SpringVersion.getVersion());
 		SpringApplication.run(Exec1TodoApplication.class, args);
 	}
 
